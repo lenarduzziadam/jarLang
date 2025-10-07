932b9ef30d8d6d4ee1a130300a6490e697644f95
Adam Lenarduzzi <adamlenarduzzi@Adams-MacBook-Air.local>
2025-10-07 14:04:47 -0500

    added a bit more personality to the initial shell messages

--- jarlang-java/Main.java
+++ jarlang-java/Main.java
@@ -7,7 +7,7 @@ import lib.JarlangShell;
 public class Main {
     public static void main(String[] args) {
         System.out.println("=== Jarlang Programming Language ===");
-        System.out.println("A warrior-themed programming language");
+        System.out.println("An Interpreter for seasoned jars!!!\nHUZZAHHH! EN GARDE!!!");
         System.out.println("Version: 1.0.0-alpha");
         System.out.println();
         
